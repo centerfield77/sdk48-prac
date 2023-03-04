6beb529008f029f237ccac1a75035a77f95d7423
centerfield77 <tatsunori.dia@gmail.com>
2023-03-05 01:53:14 +0900

    💄 gap

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text>A</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>B</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>C</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>D</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>E</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>F</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>G</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>H</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>I</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>J</Text>
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +42,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  box: {
+    backgroundColor: "white",
+    width: 150,
+    height: 150,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
